197411dc677e09da99d1c73455bb34317f104a02
Chetan <78348378+xLr864@users.noreply.github.com>
2022-03-11 16:33:36 +0530

    Update settings.py

--- faculty_management_system/settings.py
+++ faculty_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gi1xd)w^-9j^#cyv@of#j3@nf&z%96(m4^(s49k^cw4p@pmn(a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://poornima.azurewebsites.net/"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'faculty_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
