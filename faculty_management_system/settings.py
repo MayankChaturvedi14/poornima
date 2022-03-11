cc3f3923b916665746931d6009602a9871be4b66
Chetan <78348378+xLr864@users.noreply.github.com>
2022-03-11 16:44:43 +0530

    Update settings.py

--- faculty_management_system/settings.py
+++ faculty_management_system/settings.py
@@ -120,11 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
