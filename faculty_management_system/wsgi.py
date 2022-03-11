01f5042c9827d2f5c5d4fb1cc050c305f2abe09d
Chetan <78348378+xLr864@users.noreply.github.com>
2022-03-11 16:41:28 +0530

    Update wsgi.py

--- faculty_management_system/wsgi.py
+++ faculty_management_system/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faculty_management_system.settings')
 application = Cling(get_wsgi_application())
+application = get_wsgi_application()
 
